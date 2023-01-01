c42fd01b308eb349e2f765dc8d354ada0a64e30c
Alfonso Valenciana <valencianaad@hotmail.com>
2022-12-31 23:58:07 -0600

    Create a model for books

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+    # blank, null or default are valid options when modifying the schema
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
