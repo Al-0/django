9fb612192bb4fa9465ca565f7882cfcee9102961
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-01 18:10:10 -0600

    Overwrite save and use slugs

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -11,10 +12,15 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default='', null=False, db_index=True)
     # blank, null or default are valid options when modifying the schema
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse('book-detail', args=[self.slug])
     
 
     def __str__(self):
