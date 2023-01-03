f5225fcc3757e2075ca74f2fa1d0342c137bb011
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-02 18:02:32 -0600

    Change the default related name for the foreignKey

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default='', blank=True, null=False, db_index=True)
     # blank, null or default are valid options when modifying the schema
