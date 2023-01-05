2565256f088533e651cd09197c21060b0e3d11e6
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-04 21:44:14 -0600

    Add and address one-to-one field

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -11,3 +11,5 @@ class BookAdmin(admin.ModelAdmin):
   # ordering = ['title']
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,9 +5,24 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f'{self.street} {self.postal_code}, {self.city}'
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Book(models.Model):
@@ -29,4 +44,4 @@ class Book(models.Model):
     
 
     def __str__(self):
-        return f"{self.title} ({self.rating})"
+        return f'{self.title} ({self.rating})'
