835ebbe65e8e6a903e5deb04a28c28eafa5c5bb2
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-07 23:13:11 -0600

    Set up models

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Tag, Author, Post
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Post)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, related_name="posts", null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
