7e1fc0301fb4cd316279659132bf9b8ef6351fdc
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-18 23:23:12 -0600

    Customize form a bit

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label='Your name', max_length=80, error_messages={
+        'required': 'Your name must not be empty',
+        'max_length': 'Names must not have more than 80 characters'
+    })
