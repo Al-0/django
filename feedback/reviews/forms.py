9205220a8d1008f4d0f804876a8af596312fea34
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-21 20:45:14 -0600

    Change into a modelForm

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,9 +1,27 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label='Your name', max_length=80, error_messages={
-        'required': 'Your name must not be empty',
-        'max_length': 'Names must not have more than 80 characters'
-    })
-    review_text = forms.CharField(label='Your feedback', widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label='Your rating', min_value=1, max_value=6)
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your name', max_length=80, error_messages={
+#         'required': 'Your name must not be empty',
+#         'max_length': 'Names must not have more than 80 characters'
+#     })
+#     review_text = forms.CharField(label='Your feedback', widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label='Your rating', min_value=1, max_value=6)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'user_name': 'Your name',
+            'review_text': 'Your feedback',
+            'rating': 'Your rating'
+        }
+        error_messages = {
+            'user_name': {
+                'required': 'Your name must not be empty',
+                'max_length': 'Names must not have more than 80 characters'
+            }
+        }
