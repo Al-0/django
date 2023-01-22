71a81f0b1a8035435aa2cc4a013ce34770cd8b80
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-21 20:02:09 -0600

    Create a model for our form

--- feedback/reviews/models.py
+++ feedback/reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=80)
+    review_text = models.TextField()
+    rating = models.IntegerField()
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,12 +2,18 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 def review(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
+            review = Review(
+                user_name=form.cleaned_data['user_name'], 
+                review_text=form.cleaned_data['review_text'], 
+                rating=form.cleaned_data['rating'])
+            review.save()
             print(form.cleaned_data)
             return HttpResponseRedirect('/thank-you')
     else:
