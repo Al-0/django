70d8bc2e6882a550fe75757c0fcb1a403664e194
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-23 09:02:04 -0600

    Use TemplateViews

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 class ReviewView(View):
@@ -43,5 +45,23 @@ class ReviewView(View):
 #         'form': form
 #     })
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+# def thank_you(request):
+#     return render(request, 'reviews/thank_you.html')
+
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This is a message'
+        return context
+
+    
+class ReviewsListView(TemplateView):
+    template_name = 'reviews/review_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
