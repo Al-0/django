c70a35a57c17d0995319c5c8f825b07d60295c43
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-27 00:06:02 -0600

    Use a form with a file field

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect 
+
+from .forms import ProfileForm
 
 # Create your views here.
+
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            store_file(request.FILES['image'])
+            return HttpResponseRedirect('/profiles')
+        else:
+            return render(request, "profiles/create_profile.html", {
+                "form": submitted_form
+            })
