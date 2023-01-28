1ded1fe887323885da9a094e83d11fe5fc559723
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-28 00:18:15 -0600

    Use ImageField for form and model

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -3,13 +3,14 @@ from django.views import View
 from django.http import HttpResponseRedirect 
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
@@ -23,7 +24,9 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES['image'])
+            # store_file(request.FILES['image'])
+            profile = UserProfile(image = request.FILES['user_image'])
+            profile.save()
             return HttpResponseRedirect('/profiles')
         else:
             return render(request, "profiles/create_profile.html", {
