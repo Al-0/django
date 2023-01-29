d92727dd21048aa9ab86f4901be163951af55782
Alfonso Valenciana <valencianaad@hotmail.com>
2023-01-28 20:12:00 -0600

    Use CreateView to simplify view

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect 
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -13,22 +14,29 @@ from .models import UserProfile
 #             dest.write(chunk)
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-        if submitted_form.is_valid():
-            # store_file(request.FILES['image'])
-            profile = UserProfile(image = request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect('/profiles')
-        else:
-            return render(request, "profiles/create_profile.html", {
-                "form": submitted_form
-            })
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES['image'])
+#             profile = UserProfile(image = request.FILES['user_image'])
+#             profile.save()
+#             return HttpResponseRedirect('/profiles')
+#         else:
+#             return render(request, "profiles/create_profile.html", {
+#                 "form": submitted_form
+#             })
+
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
