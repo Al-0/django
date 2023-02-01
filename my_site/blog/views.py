a82678bf90b552f6fda5861835b57865e70e3021
Alfonso Valenciana <valencianaad@hotmail.com>
2023-02-01 14:56:25 -0600

    Change function views to ClassViews

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 # Create your views here.
 
-def starting_page(request):
-    lastest_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, 'blog/index.html', {
-        'posts': lastest_posts
-    })
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
 
-def posts(request):
-    all_posts = Post.objects.all()
-    return render(request, 'blog/all-posts.html', {
-        'posts': all_posts
-    })
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        data = query_set[:3]
+        return data
 
-def post_detail(request, slug):
-    single_post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html', {
-        'post': single_post,
-        'tags': single_post.tags.all()
-    })
+
+class AllPostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = self.object.tags.all()
+        return context
