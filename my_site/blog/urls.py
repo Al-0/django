2e9a167ac54683a8a335906b2bcc7975603b81ce
Alfonso Valenciana <valencianaad@hotmail.com>
2023-03-08 16:23:20 -0600

    Create the read Later section/functionality

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.StartingPageView.as_view(), name='starting-page'),
     path('posts', views.AllPostsView.as_view(), name='posts-page'),
-    path('posts/<slug:slug>', views.PostDetailView.as_view(), name='post-detail-page')
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name='post-detail-page'),
+    path('read-later', views.ReadLaterView.as_view(), name='read-later')
 ]
