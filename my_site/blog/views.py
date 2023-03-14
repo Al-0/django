c5394b73d8323a5c5e6ae0e4b8ec1a7b1fad51ef
Alfonso Valenciana <valencianaad@hotmail.com>
2023-03-13 22:19:44 -0600

    Wrap up Read Later funcitonality

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -29,13 +29,23 @@ class AllPostsView(ListView):
 
 
 class PostDetailView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        return is_saved_for_later        
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             'post': post,
             'tags': post.tags.all(),
             'comment_form': CommentForm(),
-            'comments': post.comments.all().order_by('-id')
+            'comments': post.comments.all().order_by('-id'),
+            'saved_for_later': self.is_stored_post(request, post.id)
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -54,7 +64,8 @@ class PostDetailView(View):
             'post': post,
             'tags': post.tags.all(),
             'comment_form': comment_form,
-            'comments': post.comments.all().order_by('-id')
+            'comments': post.comments.all().order_by('-id'),
+            'saved_for_later': self.is_stored_post(request, post.id)
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -84,6 +95,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session['stored_posts'] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+            
+        request.session['stored_posts'] = stored_posts
 
         return HttpResponseRedirect('/')
